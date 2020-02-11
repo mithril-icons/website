193b28d1a507fae67fab980a3aa380287a5ddb82
Michal Kijowski <contact@michalkijowski.pl>
2020-02-11 14:41:40 +0100

    Pull updated README's

--- src/_scripts/generate-readmes.ts
+++ src/_scripts/generate-readmes.ts
@@ -9,12 +9,10 @@ readmeFiles.write('const readmes: Map<string,string> = new Map()\n')
 packages.map(pkg => {
   const file = path.join(__dirname, '../..', 'node_modules/@mithril-icons', pkg.raw) + '/README.md'
   const readme = fs.readFileSync(file, { encoding: 'utf-8' })
-  // readmeFiles.write(`export const ${pkg.identifier}: string = ${JSON.stringify(readme)}\n`)
   readmeFiles.write(`readmes.set('${pkg.raw}', ${JSON.stringify(readme)})\n`)
 })
 
 const readme = fs.readFileSync(path.join(__dirname, '../..', 'README.md'), { encoding: 'utf-8' })
-// readmeFiles.write(`export const builder: string = ${JSON.stringify(readme)}\n`)
 readmeFiles.write(`readmes.set('builder', ${JSON.stringify(readme)})\n`)
 
 readmeFiles.write('export { readmes }\n')
